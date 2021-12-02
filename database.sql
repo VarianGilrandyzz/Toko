5399a15e573e1c6c2fa15c0a7a59142161f890bc
isak <muhammadisak.rizki@gmail.com>
2021-12-02 09:20:08 +0700

    update db

--- database.sql
+++ database.sql
@@ -11,6 +11,17 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Dumping structure for table ladida.barang
+DROP TABLE IF EXISTS `barang`;
+CREATE TABLE IF NOT EXISTS `barang` (
+  `id_barang` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_barang` varchar(30) NOT NULL,
+  `harga` int(5) NOT NULL,
+  `deskripsi` varchar(100) DEFAULT NULL,
+  `gambar` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_barang`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
 -- Dumping data for table ladida.barang: ~2 rows (approximately)
 /*!40000 ALTER TABLE `barang` DISABLE KEYS */;
 REPLACE INTO `barang` (`id_barang`, `nama_barang`, `harga`, `deskripsi`, `gambar`) VALUES
@@ -18,9 +29,31 @@ REPLACE INTO `barang` (`id_barang`, `nama_barang`, `harga`, `deskripsi`, `gambar
 	(4, 'Tabung Gas Elpiji', 19000, 'Tabung Gas Hijau Elpiji 3Kg', '1638389088Aquades.jpg');
 /*!40000 ALTER TABLE `barang` ENABLE KEYS */;
 
+-- Dumping structure for table ladida.detail_pemesanan
+DROP TABLE IF EXISTS `detail_pemesanan`;
+CREATE TABLE IF NOT EXISTS `detail_pemesanan` (
+  `id_pemesanan` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `jumlah` int(2) NOT NULL,
+  KEY `detail_to_pemesanan` (`id_pemesanan`),
+  KEY `detail_to_barang` (`id_barang`),
+  CONSTRAINT `detail_to_barang` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `detail_to_pemesanan` FOREIGN KEY (`id_pemesanan`) REFERENCES `pemesanan` (`id_pemesanan`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 -- Dumping data for table ladida.detail_pemesanan: ~0 rows (approximately)
 /*!40000 ALTER TABLE `detail_pemesanan` DISABLE KEYS */;
 /*!40000 ALTER TABLE `detail_pemesanan` ENABLE KEYS */;
+
+-- Dumping structure for table ladida.pembeli
+DROP TABLE IF EXISTS `pembeli`;
+CREATE TABLE IF NOT EXISTS `pembeli` (
+  `id_pembeli` int(2) NOT NULL AUTO_INCREMENT,
+  `nama_lengkap` varchar(25) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `no_telp` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_pembeli`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 -- Dumping data for table ladida.pembeli: ~1 rows (approximately)
 /*!40000 ALTER TABLE `pembeli` DISABLE KEYS */;
@@ -28,9 +61,38 @@ REPLACE INTO `pembeli` (`id_pembeli`, `nama_lengkap`, `alamat`, `no_telp`) VALUE
 	(3, 'Isyak Rizqi', 'Banyuwangi', '87762680737');
 /*!40000 ALTER TABLE `pembeli` ENABLE KEYS */;
 
+-- Dumping structure for table ladida.pemesanan
+DROP TABLE IF EXISTS `pemesanan`;
+CREATE TABLE IF NOT EXISTS `pemesanan` (
+  `id_pemesanan` int(11) NOT NULL AUTO_INCREMENT,
+  `tgl_pemesanan` date NOT NULL,
+  `total_biaya` int(6) NOT NULL,
+  `status` int(1) DEFAULT '0',
+  `id_pembeli` int(11) NOT NULL,
+  PRIMARY KEY (`id_pemesanan`),
+  KEY `pemesanan_pembeli` (`id_pembeli`),
+  CONSTRAINT `pemesanan_pembeli` FOREIGN KEY (`id_pembeli`) REFERENCES `pembeli` (`id_pembeli`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 -- Dumping data for table ladida.pemesanan: ~0 rows (approximately)
 /*!40000 ALTER TABLE `pemesanan` DISABLE KEYS */;
 /*!40000 ALTER TABLE `pemesanan` ENABLE KEYS */;
+
+-- Dumping structure for table ladida.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `is_admin` tinyint(1) DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Dumping data for table ladida.users: ~3 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
